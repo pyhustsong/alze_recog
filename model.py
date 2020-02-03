7487dbe4aaf671b85c1476aca14d970e4a21a144
NullmaxSong <sding@nullmax.ai>
2020-02-03 23:17:17 +0800

    fix bugs.

--- model.py
+++ model.py
@@ -12,24 +12,29 @@ class RecogNet(nn.Module):
             nn.Conv2d(10,20,3,1,1),
             nn.ReLU(),
             nn.MaxPool2d(2,2),
-            nn.Conv2d(20,40,3,1,1),
+            nn.Conv2d(20,20,3,1,1),
             nn.ReLU(),
             nn.MaxPool2d(2,2)
         )
-        self.rnn = nn.LSTMCell(36,112)
-        self.fc = nn.Linear(112,3)
-        self.h0 = torch.randn(112)
-        self.c0 = torch.randn(112)
+        self.rnn = nn.LSTMCell(36*20,36*20)
+        self.fc = nn.Linear(36*20,3)
+        self.h0 = torch.randn(112).cuda()
+        self.c0 = torch.randn(112).cuda()
 
     def forward(self,imgs):
         h,c = self.h0, self.c0
-        for img in imgs:
-            feature = self.feature(img)
-            feature = feature.view(-1,36)
-            h,c = self.rnn(img,(h,c))
+        channel = imgs.shape[1]
+        for i in range(channel):
+            feature = self.feature(imgs[:,i,...].unsqueeze(dim=1))
+            print(feature.shape)
+            feature = feature.view(36*40,-1)
+            h,c = self.rnn(feature,(h,c))
         output = torch.softmax(self.fc(h))
         return output
 
 if __name__ == '__main__':
     model = RecogNet()
+    model.cuda()
     print(model)
+    imgs = torch.randn(2,3,48,48).cuda()
+    print(model(imgs))
